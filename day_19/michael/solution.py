2850460e2683be42f185017a3736ad336c264f6d
MW <michael.willig@gs1.nl>
2019-11-27 09:35:50 +0100

    made template and folder structure

---
+++ day_19/michael/solution.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import math
+from collections import Counter, ChainMap, defaultdict
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
+os.chdir(CURRENT_DIRECTORY)
+
+def read_input_lines():
+    with open('input.txt', 'r') as fh:
+        return [x.strip() for x in fh.readlines]
+
+def read_input_text():
+    with open('input.txt', 'r') as fh:
+        return  fh.read().strip()
+
+
+def part_a():
+    pass
+
+def part_b():
+    pass
---
+++ make_folder_structure.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from shutil import copyfile
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
+os.chdir(CURRENT_DIRECTORY)
+
+for day in range(1,26):
+    folder_name = f'day_{str(day).zfill(2)}'
+    try:
+        os.mkdir(folder_name)
+    except FileExistsError:
+        print(f'{folder_name} already existed')
+
+    subfoldername = f'{folder_name}{os.sep}michael'
+
+    try:
+        os.mkdir(subfoldername)
+    except FileExistsError:
+        print(f'{folder_name} already existed')
+
+    copyfile('michael_template.py',f'{folder_name}{os.sep}michael{os.sep}solution.py')
+
