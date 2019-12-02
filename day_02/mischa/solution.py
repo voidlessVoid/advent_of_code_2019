8c64ef2d60be65cf53c6acb0cd36e9cc2820b0e6
voidlessVoid <58170899+voidlessVoid@users.noreply.github.com>
2019-12-02 08:35:10 +0100

    Update solution.py

--- day_02/mischa/solution.py
+++ day_02/mischa/solution.py
@@ -1,29 +1,37 @@
-import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+data = open('day02_input.txt')
+lines = data.readline().split(',')
+lines1 = [int(x) for x in lines if x.isnumeric()]
 
-CURRENT_DIRECTORY = os.path.dirname(__file__)
-os.chdir(CURRENT_DIRECTORY)
+def part_1(inp, noun, verb):
+    copy = inp.copy()
+    i = 0
+    copy[1] = int(noun)
+    copy[2] = int(verb)
+    while i <= len(copy)-4:
+        loc1, loc2, new_loc = copy[i + 1], copy[i + 2], copy[i + 3]
+        if copy[i] == 99:
+            break
+        elif copy[i] == 1:
+            new = copy[loc1] + copy[loc2]
+            copy[new_loc] = new
+            i +=4
+        elif inp[i] == 2:
+            new = copy[loc1] * copy[loc2]
+            copy[new_loc] = new
+            i += 4
+    return copy[0]
+print(part_1(lines1,12,2))
 
-def read_input_lines():
-    with open('input.txt', 'r') as fh:
-        return [x.strip() for x in fh.readlines()]
-
-def read_input_text():
-    with open('input.txt', 'r') as fh:
-        return fh.read().strip()
-
-
-def part_a():
-    pass
-
-def part_b():
-    pass
+def part_2(inp):
+    score = 19690720
+    n,v,sc=0,0,0
+    while sc < score:
+        n += 1
+        sc = part_1(inp,n,v)
+    n -=1
+    sc=0
+    while sc < score:
+        v += 1
+        sc = part_1(inp, n, v)
+    return (n,v),(n*100+v)
+print(part_2(lines1))
