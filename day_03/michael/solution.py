5f03824d7a761ff302a216f3173279b3352d6afa
MW <michael.willig@gs1.nl>
2019-12-03 06:47:24 +0100

    day 3

--- day_03/michael/solution.py
+++ day_03/michael/solution.py
@@ -23,7 +23,50 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    DIRS = {"L":1j,"R":-1j,"U": -1,"D":1}
+    wire_1, wire_2 = read_input_lines()
 
+    def dir_list_to_set(dir_list):
+        s = set()
+        loc = 0
+        for instruction in dir_list:
+            dir = instruction[0]
+            steps = int(instruction[1:])
+            for step in range(steps):
+                loc += DIRS[dir]
+                s.add(loc)
+        return s
+
+    wire_1_set = dir_list_to_set(wire_1.split(','))
+    wire_2_set = dir_list_to_set(wire_2.split(','))
+    crossings = wire_1_set & wire_2_set
+    min_dist = min(x.real + x.imag for x in crossings )
+    print(min_dist)
+
+
+part_a()
 def part_b():
-    pass
+    DIRS = {"L": 1j, "R": -1j, "U": -1, "D": 1}
+    wire_1, wire_2 = read_input_lines()
+
+    def dir_list_to_dict(dir_list):
+        d = {}
+        loc = 0
+        i=0
+        for instruction in dir_list:
+            dir = instruction[0]
+            steps = int(instruction[1:])
+            for step in range(steps):
+                loc += DIRS[dir]
+                i+=1
+                if not loc in d:
+                    d[loc] = i
+        return d
+
+    wire_1_set = dir_list_to_dict(wire_1.split(','))
+    wire_2_set = dir_list_to_dict(wire_2.split(','))
+    crossings = set(wire_1_set) & set(wire_2_set)
+    min_dist = min(wire_1_set[x]+wire_2_set[x] for x in crossings)
+    print(min_dist)
+
+part_b()
