2dc8d37cdcab342f2776c724cd245847302f822a
MW <michael.willig@gs1.nl>
2019-12-04 07:27:41 +0100

    day 4

--- day_04/michael/solution.py
+++ day_04/michael/solution.py
@@ -23,7 +23,34 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    def valid(x):
+        t = str(x)
+        pair = any(t[i] == t[i+1] for i in range(len(t)-1))
+        order = t == ''.join(sorted(t))
+        return pair and order
+    mi,ma = 278384,824795
+    print(len([x for x in range(mi, ma+1) if valid(x)]))
+#part_a()
 
 def part_b():
-    pass
+    def valid(x):
+        t = str(x)
+        order = t == ''.join(sorted(t))
+
+        countlist=[]
+        lastchar = ''
+        count = 0
+        for char in t:
+            if char == lastchar:
+                count+=1
+            else:
+                countlist.append(count)
+                lastchar = char
+                count = 1
+        countlist.append(count)
+
+        return order and 2 in countlist
+
+    mi, ma = 278384, 824795
+    print(len([x for x in range(mi, ma + 1) if valid(x)]))
+part_b()
