201a3727de9ba9872e3464c2a8c209235523be9c
MW <michael.willig@gs1.nl>
2019-12-05 14:48:17 +0100

    bit of cleaning

--- day_05/michael/solution.py
+++ day_05/michael/solution.py
@@ -24,54 +24,52 @@ def read_input_text():
 
 def part_a(inp = 1):
     outputlist = []
-    get = lambda val, mode: mode2value[mode](val)
-    def put(place,val):
-        codelist[place] = val
 
     mode2value = {
         1 : lambda x: x,
         0 : lambda x: codelist[x]
     }
+    get = lambda val, mode: mode2value[mode](val)
+    def put(place,val):
+        codelist[place] = val
 
     def add(values, modes):
-        a = get(values[0], modes.pop(0))
-        b = get(values[1], modes.pop(0))
+        a = get(values[0], modes.pop())
+        b = get(values[1], modes.pop())
         put(values[2],a+b)
 
     def mul(values, modes):
-        a = get(values[0], modes.pop(0))
-        b = get(values[1], modes.pop(0))
+        a = get(values[0], modes.pop())
+        b = get(values[1], modes.pop())
         put(values[2],a*b)
 
     def input(values, modes):
         put(values[0], inp)
 
     def output(values, modes):
-        outputlist.append(get(values[0], modes.pop(0)))
+        outputlist.append(get(values[0], modes.pop()))
 
     def JIT(values, modes):
-        if get(values[0],modes.pop(0))!= 0:
-            i[0] = get(values[1],modes.pop(0))
+        if get(values[0],modes.pop())!= 0:
+            i[0] = get(values[1],modes.pop())
             return True
 
     def JIF(values, modes):
-        if get(values[0],modes.pop(0))== 0:
-            i[0] = get(values[1],modes.pop(0))
+        if get(values[0],modes.pop())== 0:
+            i[0] = get(values[1],modes.pop())
             return True
 
     def LT(values, modes):
-        a = get(values[0], modes.pop(0))
-        b = get(values[1], modes.pop(0))
+        a = get(values[0], modes.pop())
+        b = get(values[1], modes.pop())
         if a < b:
-            #put(get(values[2], modes.pop(0)), 1)
             put(values[2],1)
         else:
-            #put(get(values[2], modes.pop(0)), 0)
             put(values[2],0)
 
     def EQ(values, modes):
-        a = get(values[0], modes.pop(0))
-        b = get(values[1], modes.pop(0))
+        a = get(values[0], modes.pop())
+        b = get(values[1], modes.pop())
         if  a == b:
             #put(get(values[2], modes.pop(0)), 1)
             put(values[2],1)
@@ -100,24 +98,19 @@ def part_a(inp = 1):
         8: 4
     }
 
-    #codelist = [int(x.strip()) for x in """3,9,7,9,10,9,4,9,99,-1,8""".split(',')]
     codelist = [int(x.strip()) for x in read_input_text().split(',')]
-
-
     i = [0]
     while codelist[i[0]] != 99:
         opcode = codelist[i[0]]
         op = int(str(opcode)[-2:])
-        modes = [int(x) for x in str(opcode)[:-2][::-1]] + ([0] * 4) #add extra 0 at the end for if fewer modes are given
+        modes = [int(x) for x in str(opcode)[:-2].zfill(4)] #add extra 0 at the front for in case modes are missing
         func = int2func[op]
         values = codelist[i[0]+1:i[0]+int2len[op]]
         jumped = func(values,modes)
         if not jumped:
             i[0]+= int2len[op]
-        print(i, codelist)
 
-
-    print(outputlist)
+    print(outputlist[-1])
 
 
 def part_b():
