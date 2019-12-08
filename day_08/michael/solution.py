f2e0cc3a8f7a84b835dc6a78d91eadc2cb65697b
MW <michael.willig@gs1.nl>
2019-12-08 15:11:16 +0100

    day 8

--- day_08/michael/solution.py
+++ day_08/michael/solution.py
@@ -23,7 +23,27 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    pixels = read_input_text()
+    width, height = 25, 6
+    layers = [pixels[i:i+width*height] for i in range(0,len(pixels),width*height)]
+    counters = [Counter(layer) for layer in layers]
+    relevant_counter = sorted(counters,key =lambda x: x['0'])[0]
+    print(relevant_counter['1']*relevant_counter['2'])
+part_a()
 
 def part_b():
-    pass
+    pixels = read_input_text()
+    width, height = 25, 6
+    layers = [pixels[i:i + width * height] for i in range(0, len(pixels), width * height)]
+
+    final_layer = ['2']*(width*height)
+    for layer in layers[::-1]:
+        for i in range(width*height):
+            if layer[i] != '2':
+                final_layer[i] = layer[i]
+
+    rows = [final_layer[i:i+width] for i in range(0,width*height,width)]
+    for row in rows:
+        print(' '.join([' ' if x == '0' else 'x' for x in row]))
+
+part_b()
