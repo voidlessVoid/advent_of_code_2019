421bc2ea7504e34bccead9c6afdeeb3eea8838e5
Michael Willig <mbwillig@hotmail.com>
2019-12-14 09:16:38 +0100

    day 14

--- day_14/michael/solution.py
+++ day_14/michael/solution.py
@@ -22,8 +22,52 @@ def read_input_text():
         return fh.read().strip()
 
 
-def part_a():
-    pass
+def part_a(req={'FUEL':1}):
+    reactions =[["".join([y for y in x if y.isalnum()]) for x in z.split()] for z in read_input_lines()]
+    reactions = [[x for x in y if x] for y in reactions] #remove empty strings
+    reactiondict = {}
+    for reaction in reactions:
+        x = list(zip(reaction[1::2],[int(x) for x in reaction[::2]]))
+        reactiondict[x[-1][0]] = (x[-1][1],x[:-1]) #every product linked to a tuple of # of product and ingredients
+
+    wishlist = req
+    extra_coomponents=defaultdict(int)
+    while set(wishlist) - {'ORE'}:
+        for item in list(wishlist.keys()):
+            if item != 'ORE':
+                if wishlist[item] < extra_coomponents[item]:
+                    extra_coomponents[item] -= wishlist[item]
+                else:
+                    required = wishlist[item] - extra_coomponents[item]
+                    extra_coomponents[item] = 0
+                    reaction = reactiondict[item]
+                    runs = math.ceil(required/reaction[0])
+                    produced = runs * reaction[0]
+                    extra_coomponents[item] += produced-required
+
+                    for ingredient, quantity in reaction[1]:
+                        if ingredient in wishlist:
+                            wishlist[ingredient]+= quantity * runs
+                        else:
+                            wishlist[ingredient] = quantity * runs
+                del wishlist[item]
+    return(wishlist['ORE'])
+
+print(part_a())
+
+
+
 
 def part_b():
-    pass
+    ore = 1000000000000
+    estimate_window = [ore//248794,ore]
+    while estimate_window[0] != estimate_window[1]:
+        middle = math.ceil(sum(estimate_window)/2)
+        ore_req = part_a({"FUEL":middle})
+        if ore_req > ore:
+            estimate_window[1] = middle-1
+        else:
+            estimate_window[0] = middle
+    print(estimate_window[0])
+
+part_b()
